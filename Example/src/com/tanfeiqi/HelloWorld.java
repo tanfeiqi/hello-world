f63f1f058b8fa70bbee74b78291eac6dcc708722
feiqi <feiqi@mogujie.com>
2018-01-05 00:05:06 +0800

    add form mac

--- Example/src/com/tanfeiqi/HelloWorld.java
+++ Example/src/com/tanfeiqi/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.tanfeiqi;
 
+
 public class HelloWorld {
 
     public static void main(String[] args) {
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # hello-world
 just start
 2017.11.30
+
+笔记:
+1, src必须标记为source root
