97023bb6d432b4c78972b4d51f9ac9322e6f6ffe
feiqi <feiqi@mogujie.com>
2018-01-06 16:13:39 +0800

    add

---
+++ Example/src/com/tanfeiqi/Car.java
@@ -0,0 +1,20 @@
+package com.tanfeiqi;
+
+/**
+ * Created by tan on 2018/1/6.
+ */
+public class Car {
+
+    /**
+     * 类: 事物的描述, 属性&行为
+     * 对象: 类的实例
+     */
+
+    int num;
+
+    String color;
+
+    void run() {
+        System.out.println(num + "," + color);
+    }
+}
--- Example/src/com/tanfeiqi/HelloWorld.java
+++ Example/src/com/tanfeiqi/HelloWorld.java
@@ -5,5 +5,32 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+
+        //一维数组
+        int[] arr = new int[4];
+        System.out.println(arr);  //@右边是实体的hash值
+        System.out.println(arr.length);
+
+        //二维数组
+        int[][] b = new int[3][2];
+        b[1][1] = 4;
+        System.out.println(b);
+        System.out.println(b.toString());
+
+        //二维不确定数组
+        int[][] c = new int[3][];
+        System.out.println(c);
+        System.out.println(c[0]); // null
+        //System.out.println(c[0][0]);  //null point
+
+        int[][] d = {{3,2,10,15}, {12,23,1}, {1,2,12}};
+        System.out.println(d.length);
+        System.out.println(d[0].length);
+        for(int i=0; i<d.length; i++) {
+            for (int j=0; j<d[i].length; j++) {
+                System.out.print(d[i][j] + ",");
+            }
+        }
     }
 }
+
